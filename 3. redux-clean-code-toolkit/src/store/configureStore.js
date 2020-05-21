b054ddfce511bd1e96a38d640d1232637ef9847a
Md Hasibul Hossain <hasibdesk@gmail.com>
2020-05-22 02:14:10 +0600

    Added new store callled Projects and reducers

--- 3. redux-clean-code-toolkit/src/store/configureStore.js
+++ 3. redux-clean-code-toolkit/src/store/configureStore.js
@@ -1,10 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import reducer from './bugs';
+import projects from './projects';
 
 export default function () {
 	const store = configureStore({
-		reducer: reducer,
+		reducer: projects,
 	});
 	return store;
 }
